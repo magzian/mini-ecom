1d8488a529e5aa821ab3bf0262e274bd7c0cb747
magzian <ianbenard2002@gmail.com>
2025-09-06 14:19:11 +0300

    created dashboard route

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,12 +3,14 @@ import LoginView from '../views/LoginView.vue';
 import RegisterView from '../views/RegisterView.vue';
 
 import { auth } from './stores/auth'
+import DashboardView from '../views/DashboardView.vue';
 
 
 
 const routes = [
   { path: '/login', component: LoginView },
   { path: '/register', component: RegisterView },
+  {  path:'/dashboard', component:DashboardView }
 ];
 
 export default createRouter({
